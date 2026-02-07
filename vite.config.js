e2e406a7d0fa239febbcdf12df61a43821893324
briamstechnologies <briamstechnologies@gmail.com>
2026-02-08 00:14:26 +0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-// vite.config.js
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") },
   },
+  server: {
+    allowedHosts: ["curevirtual-2-frontend-production.up.railway.app"],
+  },
 });
-
